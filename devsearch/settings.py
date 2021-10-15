ed787e47c1c03789d039c2a1d89ed7d02e72593b
Md. Mesbahul Islam <82472635+mesba-islam@users.noreply.github.com>
2021-10-15 17:13:12 +0600

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -193,8 +193,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mesbansu@gmail.com'
-EMAIL_HOST_PASSWORD = 'm3s21000'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'Pass'
 
 
 # Static files (CSS, JavaScript, Images)
